b043b3423bd79340877859796880fa53df782e3b
Josh Feinstein <joshua.scott.feinstein@gmail.com>
2020-04-01 20:10:15 -0400

    configures react, webpack, gulp

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const exec = require("child_process").exec;
+
+
+/** WEBPACK BUILD **/
+
+/* runs `npm run build` script */
+gulp.task("webpack:build", function(cb) {
+  exec("npm run build", function(err, stdout, stderr) {
+    console.log(stdout);
+    console.log(stderr);
+    cb(err);
+  });
+});
+
+
+/**  WATCH **/
+
+/* watches all assets for changes and runs sub-tasks */
+gulp.task("watch", function() {
+  gulp.watch("app/**/*", gulp.series("webpack:build"));
+});

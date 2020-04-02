587001ebe49c1309741bd79485907cf318234683
Josh Feinstein <joshua.scott.feinstein@gmail.com>
2020-04-01 20:45:42 -0400

    configures redux

--- app/src/index.js
+++ app/src/index.js
@@ -1,5 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './screens/App.js';
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import configureStore from './configureStore'
+import App from './screens/App'
 
-ReactDOM.render(<App />, document.getElementById('app'));
+const store = configureStore();
+
+const renderApp = () =>
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    document.getElementById('root')
+  )
+
+if (process.env.NODE_ENV !== 'production' && module.hot) {
+  module.hot.accept('./screens/App', renderApp);
+}
+
+renderApp();

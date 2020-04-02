e1ec97d62b7e9ce7e0a636445c4037eeb7808b01
Josh Feinstein <joshua.scott.feinstein@gmail.com>
2020-04-01 21:00:40 -0400

    adds hashrouter and multiple screens and components

--- app/src/screens/App.js
+++ app/src/screens/App.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react';
-import TestScreen from "./TestScreen";
+import { HashRouter, Route, Switch } from 'react-router-dom'
+
+import Home from "./Home";
+import About from "./About";
+import NotFound from "./NotFound";
+import UserProfile from "./UserProfile";
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
 
 
 class App extends Component{
    render(){
       return(
-         <div>
-            <h1>Hello Arcade World!</h1>
-            <TestScreen />
-         </div>
+        <HashRouter>
+          <div>
+            <nav>
+              <Navbar />
+            </nav>
+            <main>
+              <Switch>
+                <Route exact path='/' component={Home} />
+                <Route exact path='/about' component={About} />
+                <Route exact path='/profile' component={UserProfile} />
+                <Route component={NotFound} />
+              </Switch>
+              <Footer />
+            </main>
+          </div >
+        </HashRouter>
       );
    }
 }
